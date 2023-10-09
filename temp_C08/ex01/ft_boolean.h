01a46af7aaae95e424945c6cc40c1fbd6a2e7ed3
Vittoria Oliviero <volivier@e3r4p8.42roma.it>
2023-10-09 17:45:08 +0200

    push di c08

---
+++ temp_C08/ex01/ft_boolean.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volivier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 16:31:33 by volivier          #+#    #+#             */
+/*   Updated: 2023/10/09 17:28:06 by volivier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef enum a_bool
+{
+	true = 1,
+	false = 0,
+}	t_bool;
+
+# define TRUE true
+# define FALSE false
+# define ODD_MSG "I have an odd number of arguments."
+# define EVEN_MSG "I have an even number of arguments."
+# define SUCCESS 0
+
+# define EVEN (int) (int % 2 == 0)
+
+#endif
