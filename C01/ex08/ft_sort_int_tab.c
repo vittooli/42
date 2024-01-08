834070a9f7477ea54106f6297fbb75783bd0d112
vittooli <146972863+vittooli@users.noreply.github.com>
2024-01-08 11:26:16 +0100

    Add files via upload

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volivier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/24 12:45:45 by volivier          #+#    #+#             */
+/*   Updated: 2023/09/25 12:46:09 by volivier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+	int	min;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		min = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[j] < tab[min])
+				min = j;
+			j++;
+		}
+		temp = tab[i];
+		tab[i] = tab[min];
+		tab[min] = temp;
+		i++;
+	}
+}
